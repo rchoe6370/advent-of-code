c4e2238f1060ed19e362fd46d2d19d256a2907f9
Ryan <ryc99@pitt.edu>
2025-12-14 20:42:53 -0500

    day1

---
+++ day1/Day1.java
@@ -0,0 +1,49 @@
+package day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("input.txt");
+        Scanner sc = new Scanner(file);
+
+        int zeroCount = 0;
+        int pos = 50;
+
+        while(sc.hasNextLine()) {
+
+            String line = sc.nextLine(); 
+
+            String direction = line.substring(0, 1);
+            int rotation = Integer.valueOf(line.substring(1));
+            
+            if(direction.equals("L")) {
+                pos = pos - rotation;
+                if(pos < 0) {
+                    pos = 100 + pos;
+                }
+            } else {
+                pos = pos + rotation;
+                if(pos > 99) {
+                    pos = pos - 100;    
+                }
+            }
+
+
+            if(pos == 0) {
+                zeroCount++;
+            }
+
+        
+        }
+
+        System.out.println("Password: " + zeroCount);
+
+        sc.close();
+    }
+
+
+}
